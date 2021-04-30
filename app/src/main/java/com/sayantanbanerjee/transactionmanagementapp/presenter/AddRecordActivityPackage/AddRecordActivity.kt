2492772a393e057f0ad676a2c28feca886870a46
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-04-30 18:39:35 +0530

    Successfully generating QR Code

--- app/src/main/java/com/sayantanbanerjee/transactionmanagementapp/presenter/AddRecordActivityPackage/AddRecordActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/transactionmanagementapp/presenter/AddRecordActivityPackage/AddRecordActivity.kt
@@ -1,11 +1,15 @@
 package com.sayantanbanerjee.transactionmanagementapp.presenter.AddRecordActivityPackage
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.sayantanbanerjee.transactionmanagementapp.R
+import com.sayantanbanerjee.transactionmanagementapp.data.preference.AppPreferenceHelper
 import com.sayantanbanerjee.transactionmanagementapp.databinding.ActivityAddRecordBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -14,6 +18,9 @@ import javax.inject.Inject
 class AddRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddRecordBinding
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     @Inject
     lateinit var factory: AddRecordViewModelFactory
@@ -30,17 +37,46 @@ class AddRecordActivity : AppCompatActivity() {
             val phoneNumber = binding.phoneNumber.text.toString()
             val amount = binding.amountInvolved.text.toString()
             var parity = 1
+            var qrParity: Int = 0
             if (binding.parityGroup.checkedRadioButtonId == R.id.amountReceivedButton) {
                 parity = 0
+                qrParity = 1
             }
 
             if (phoneNumber == "" || amount == "") {
                 Toast.makeText(this, "Fields cannot be left empty!", Toast.LENGTH_SHORT).show()
             } else {
-                viewModel.saveRecord(phoneNumber, amount.toInt(),  parity)
-                Toast.makeText(this, "Saved in Database!", Toast.LENGTH_SHORT).show()
-            }
 
+                viewModel.saveRecord(phoneNumber, amount.toInt(), parity)
+
+                val minePhoneNumber = AppPreferenceHelper(sharedPreferences).getUserMobileNumber()
+                var inputForQREncryption = ""
+                inputForQREncryption += "QRTRANSACTION;"
+                inputForQREncryption += minePhoneNumber
+                inputForQREncryption += ";"
+                inputForQREncryption += phoneNumber
+                inputForQREncryption += ";"
+                inputForQREncryption += amount
+                inputForQREncryption += ";"
+                inputForQREncryption += qrParity.toString()
+                inputForQREncryption += ";"
+                inputForQREncryption += "2"
+                inputForQREncryption += ";"
+
+                try {
+                    val barcodeEncoder = BarcodeEncoder()
+                    val bitmap = barcodeEncoder.encodeBitmap(inputForQREncryption, BarcodeFormat.QR_CODE, 600, 600)
+                    binding.qrCode.setImageBitmap(bitmap)
+                    Toast.makeText(this, "QR Successfully generated", Toast.LENGTH_LONG).show()
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        this,
+                        "Something went wrong in generating QR Code!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            }
         }
     }
 }
